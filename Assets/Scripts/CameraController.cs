d875c001a15e29a62dce53d15ccc25f399c39832
r889s843 <ryan.strong@ku.edu>
2024-10-23 12:46:20 -0500

    basic level

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero; 
+    [SerializeField] private Transform player;
+
+    private void Update() {
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z, ref velocity, speed * Time.deltaTime));
+
+        //follow player
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+}
