0e190475465a9e0c69e0808d6af2e8d7d01af647
Chris Harvey <37196036+Christoper-Harvey@users.noreply.github.com>
2024-11-24 19:23:46 -0600

    fixed music, AI, sfx, and level bugs

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,3 +1,8 @@
+// Name: Chris Harvey, Ian Collins, Ryan Strong, Henry Chaffin, Kenny Meade
+// Date: 11/24/2024
+// Course: EECS 581
+// Purpose: Creates music player for background track that persists
+
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
