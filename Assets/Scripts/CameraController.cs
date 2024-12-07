24e5a6c00bcd458b22adecf42b9adc98cb10c97f
r889s843 <ryan.strong@ku.edu>
2024-12-07 16:41:36 -0600

    player 2 camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,16 +7,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Camera mainCamera; //camera game object
     [SerializeField] private Transform player;
     [SerializeField] private float aheadDistance; //distance that camera will look ahead
     [SerializeField] private float aboveDistance; //distance that cameral will look above
-    //[SerializeField] private float cameraSpeed; //camera speed for switching look ahead directions
-    //private float lookAhead; //distance and direction for camera to look ahead and not be centered on player
+
+    //2 player variables
+    private Transform player2; //player2's transform
+    public bool player2Active; //tracks whether game is single or 2 player at the moment
+    // [SerializeField] private float edgeBuffer; //space of player to edge
+    [SerializeField] private float minZoom = 5.0f; //min size of camera
+    [SerializeField] private float maxZoom = 20.0f; //max size of camera
+    [SerializeField] private float zoomSpeed = 0.2f; //zoom speed
+    private float smoothTime;//
+
 
     private void Awake() {
-        //sets camera position to correct lookahead on startup FOR OLD CAMERA CONTROLLER
-        //lookAhead = aheadDistance * player.localScale.x;
-        //transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        mainCamera = Camera.main;
 
         //if player object is empty then find it
         if(player == null) {
@@ -25,12 +32,51 @@ public class CameraController : MonoBehaviour
     }
 
     private void Update() {
-        //follow player
-        transform.position = new Vector3(player.position.x + aheadDistance, player.position.y + aboveDistance, transform.position.z);
+        //get player 2 transform right as its activated
+        if(player2 == null && player2Active) { //executes only once
+            player2 = GameObject.Find("Player2(Clone)").transform; //assign player2's transform
+        }
 
+        //make camera follow player
+        if(player2Active) { //multiplayer
+            Vector3 avgPos = getNewPosition(); //get average position
+            transform.position = new Vector3(avgPos.x, avgPos.y, transform.position.z); //set new position
 
-        //OLD CAMERA CONTROLLER
-        //transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z); //actually moves camera to player's position
-        //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed); //slowly transition camera to final lookahead
+            //set new size
+            mainCamera.orthographicSize = Mathf.SmoothDamp(mainCamera.orthographicSize, getNewZoom(), ref smoothTime, zoomSpeed);
+        }
+        else { //single player
+            transform.position = new Vector3(player.position.x + aheadDistance, player.position.y + aboveDistance, transform.position.z);
+        }
+
+    }
+
+    //returns average position of both players
+    private Vector3 getNewPosition()
+    {
+        Vector3 averagePosition = new Vector3();
+
+        //add both player's positions together
+        averagePosition += player.position;
+        averagePosition += player2.position;
+
+        //calculate average
+        averagePosition /= 2;
+
+        return averagePosition;
+    }
+
+    //returns new camera zoom
+    //calculates based on distance between players
+    private float getNewZoom(){
+        float zoom = 0.0f;
+
+        //zoom is proportional to distance between players
+        zoom = Vector3.Distance(player.position, player2.position);
+
+        //restrict zoom to min and max
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+
+        return zoom;
     }
 }
--- Assets/Scripts/CoOpInitialize.cs
+++ Assets/Scripts/CoOpInitialize.cs
@@ -6,10 +6,12 @@ public class CoOpInitialize : MonoBehaviour
     private GameObject player2; //new player2's game object
     private bool activated; //tracks whether coop has been activated already or not
 
+    private CameraController cameraControllerScript; //camera controller script to tell that player 2 has been activated
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cameraControllerScript = Camera.main.GetComponent<CameraController>(); //get camera controller script
     }
 
     // Update is called once per frame
@@ -18,6 +20,9 @@ public class CoOpInitialize : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.C) && !activated) {
             player2 = Instantiate(player2Prefab, Vector3.zero, Quaternion.identity) as GameObject;
             activated = true;
+
+            //tell camera controller that player 2 is active
+            cameraControllerScript.player2Active = true;
         }
     }
 }
