bd57fca703aabbcf5c9f033b9e03eadf59438ac7
Chris Harvey <37196036+Christoper-Harvey@users.noreply.github.com>
2025-02-12 17:35:57 -0600

    added npc interface and bug fix

---
+++ Assets/NPC.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject dialoguePanel;
+    public TMPro.TextMeshProUGUI dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public GameObject continueButton;
+    public float wordSpeed;
+    public bool playerIsClose;
+
+    void Start()
+    {
+        dialogueText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && playerIsClose){
+            if(dialoguePanel.activeInHierarchy){
+                zeroText();
+            }
+            else{
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        if(dialogueText.text == dialogue[index]){
+            continueButton.SetActive(true);
+        }
+    }
+
+    public void zeroText(){
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing(){
+        foreach(char letter in dialogue[index].ToCharArray()){
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine(){
+        continueButton.SetActive(false);
+
+        if (index < dialogue.Length -1){
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        } else {
+            zeroText();
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")){
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")){
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+}
